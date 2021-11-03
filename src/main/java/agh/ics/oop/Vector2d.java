6698bfebf05a60a71da439570ead490f8015d608
Glitchmin <adam.p.kulczycki@gmail.com>
2021-11-03 21:07:46 +0100

    początek lab3

---
+++ src/main/java/agh/ics/oop/Vector2d.java
@@ -0,0 +1,53 @@
+package agh.ics.oop;
+
+public class Vector2d {
+    public Vector2d(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    final int x;
+    final int y;
+
+    public String toString() {
+        return "(" + Integer.toString(this.x) + "," + Integer.toString(y) + ")";
+    }
+
+    public boolean precedes(Vector2d other) {
+        return (this.x <= other.x && this.y <= other.y);
+    }
+
+    public boolean follows(Vector2d other) {
+        return (this.x >= other.x && this.y >= other.y);
+    }
+
+    public Vector2d upperRight(Vector2d other) {
+        return new Vector2d(Math.max(this.x, other.x), Math.max(this.y, other.y));
+    }
+
+    public Vector2d lowerLeft(Vector2d other) {
+        return new Vector2d(Math.min(this.x, other.x), Math.min(this.y, other.y));
+    }
+
+
+    public Vector2d add(Vector2d other) {
+        return new Vector2d(this.x + other.x, this.y + other.y);
+    }
+
+    public Vector2d subtract(Vector2d other) {
+        return new Vector2d(this.x - other.x, this.y - other.y);
+    }
+
+    public boolean equals(Object other) {
+        if (this == other)
+            return true;
+        if (!(other instanceof Vector2d))
+            return false;
+        Vector2d that = (Vector2d) other;
+        return (that.x == this.x && that.y == this.y);
+    }
+
+    public Vector2d opposite() {
+        return new Vector2d(-this.x, -this.y);
+    }
+}
